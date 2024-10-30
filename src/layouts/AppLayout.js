eb311758c9bf1c44d9ebfdedc3452f4a8c519118
Mohamed Elshahat <mohamed.elshahat@casita.com>
2024-10-30 18:36:46 +0300

    fix

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -9,7 +9,7 @@ export default function AppLayout() {
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Me...</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
